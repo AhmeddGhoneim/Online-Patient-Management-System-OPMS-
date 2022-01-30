d53d55a902a1b1c2a4255c89eb49ab5e8da8757d
Ahmed Ghoneim <ahmeddghoneimm@gmail.com>
2022-01-30 04:36:14 +0200

    Created The first two pages that patient will see

---
+++ Schedule.php
@@ -0,0 +1,24 @@
+<?php
+include('header.php');
+?>
+<div class="card">
+	<form>
+	    <div class="card-header"><h3><b>Doctor Schedule List</b></h3></div>
+	    <div class="card-body">
+	    	<div class="table-responsive">
+	    		<table class="table table-striped table-bordered text-center">
+	    			<tr>
+	    				<th>Doctor Name</th>
+	    				<th>Education</th>
+	    				<th>Speciality</th>
+	    				<th>Appointment Date</th>
+	    				<th>Appointment Day</th>
+	    				<th>Available Time</th>
+	    				<th>Action</th>
+	    			</tr>
+	    		</table>
+	    	</div>
+	    </div>
+	</form>
+</div>
+
---
+++ appointment.php
@@ -0,0 +1,28 @@
+<?php
+include('header.php');
+?>
+<div class="card">
+	<form>
+	    <div class="card-header"><h4><b>My Appointment List</b></h4></div>
+	    <div class="card-body">
+	    	<div class="table-responsive">
+	    		<table class="table table-striped table-bordered text-center">
+	    			<thead>
+	    			<tr>
+	    				<th>Appointment No.</th>
+	    				<th>Doctor Name</th>
+	    				<th>Appointment Date</th>
+	    				<th>Appointment Time</th>
+	    				<th>Appointment Day</th>
+	    				<th>Appointment status</th>
+	    				<th>Download</th>
+	    				<th>Cancel</th>
+	    			</tr>
+	    			</thead>
+	    			<tbody></tbody>
+	    		</table>
+	    	</div>
+	    </div>
+	</form>
+</div>
+
