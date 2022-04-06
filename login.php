8a0d50f208c65386da2547d76ccf97acafc453c6
Mahmoudalaa7 <103141216+Mahmoudalaa7@users.noreply.github.com>
2022-04-06 22:58:29 +0300

    Add files via upload
    Login page updated

--- login.php
+++ login.php
@@ -1,30 +1,34 @@
-<?php
-include('header.php');
-?>
-<div class="container">
-	<div class="row justify-content-md-center">
-		<div class="col col-md-3">
-			<div class="card">
-				<div class="card-header">SIGN IN</div>
-				<div class="card-body">
-					<form id="patient_login_form">
-						<div class="form-group">
-							<label>Patient Email Address</label>
-							<input type="text" name="Patient_email_address" id="Patient_email_address" class="Form-control" required autofocus data-parsley-type="email" data-parsley-trigger="keyup" />
-						</div> 
-						<div class="form-group">
-							<label>Patient Password</label>
-							<input type="Password" name="Patient_Password" id="Patient_Password" class="Form-control" required data-parsley-trigger="keyup" />
-						</div>
-						<div class="form-group text-center">
-							<input type="Submit" name="patient_login_button" id="patient_login_button" class="btn btn-primary" value="Login"/>
-						</div>
-						<div class="form-group text-center">
-							<p><a href="registration.php">SIGN UP</a></p>
-						</div>	
-					</form>
-				</div>		
-			</div>	
-		</div>
-	</div>
-</div>
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="lstyle.css">
+</head>
+<body>
+  <div class="header">
+      <h2>Login</h2>
+  </div>
+
+  <form method="post" action="login.php">
+  	  <?php include('errors.php'); ?>
+      <div class="input-group">
+        <label>Username</label>
+        <input type="text" name="username">
+      </div>
+      <div class="input-group">
+        <label>Password</label>
+        <input type="password" name="password">
+      </div>
+      <div class="input-group">
+        <button type="submit" name="login" class="btn">Login</button>
+      </div>
+      <p>
+          Not yet a member? <a href="Register.php">Sign up</a>
+      </p>
+  </form>
+
+  <div class="banner"></div>
+
+</body>
+</html>
