2377388d628680bbcdd542c7632e1d308ad93cba
Ahmed Ghoneim <ahmeddghoneimm@gmail.com>
2022-04-06 21:00:35 +0200

    Add files via upload

---
+++ HomePage.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UrDoctor</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="top-bar">
+			<div class="logo">
+				<img src="2.jpeg">
+			</div>
+		    <div class="menu-bar">
+				<ul>
+					<li class="active">Home</li>
+					<li>Services</li>
+               	 	<li>Blog</li>
+                	<li>About</li>
+                	<li>Contact</li>
+				</ul>
+			</div>
+			<div class="search-box">
+				<div class="input-search">
+					<input type="text" placeholder="Search">
+					<div class="search-icon">
+						<span><i class="fa fa-search"></i></span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="hero-banner">
+				<img src="img-1.jpeg">
+			</div>
+		<div class="hero">
+			<div class="text">
+				<h1>Connect Securely with telecare</h1>
+				<p>Allows doctors to examine patients online. 
+				Deliver telemedicine health services for greater reach, improved patient outcomes and lasting cost savings.</p>
+			</div>
+			<div class="but">
+				<div class="icon"><i class="fa fa-youtube"></i></div>
+				<span>Video</span>
+			</div>
+			<div class="Button">
+				<a href="Register.php">Get Started</a>
+			</div>
+		</div>	
+		<div class="shopping-box"><i class="fa fa-shopping-bag"></i></div>
+		<div class="social-icons">
+			<ul>
+				<li><i class="fa fa-facebook-square"></i></li>
+				<li><i class="fa fa-twitter-square"></i></li>
+				<li><i class="fa fa-pinterest-square"></i></li>
+				<li><i class="fa fa-instagram"></i></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
